94a0e606e03ba042d1a247505775d34cc6f0da87
MOHAMED AZIZ GALAOUI <apopolo9800@gmail.com>
2026-09-19 14:21:21 +0100

    Preserve MoneyFlow application identity while adopting structured foundation

--- app/src/main/java/com/moneyflow/data/database/MoneyFlowDatabase.kt
+++ app/src/main/java/com/moneyflow/data/database/MoneyFlowDatabase.kt
@@ -1,12 +1,12 @@
-package com.moneyflow.data.database
+package com.tropico.moneyflow.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.moneyflow.data.dao.BudgetDao
-import com.moneyflow.data.dao.ExpenseDao
-import com.moneyflow.data.entity.CategoryBudgetEntity
-import com.moneyflow.data.entity.ExpenseEntity
-import com.moneyflow.data.entity.MonthlyBudgetEntity
+import com.tropico.moneyflow.data.dao.BudgetDao
+import com.tropico.moneyflow.data.dao.ExpenseDao
+import com.tropico.moneyflow.data.entity.CategoryBudgetEntity
+import com.tropico.moneyflow.data.entity.ExpenseEntity
+import com.tropico.moneyflow.data.entity.MonthlyBudgetEntity
 
 @Database(
     entities = [ExpenseEntity::class, MonthlyBudgetEntity::class, CategoryBudgetEntity::class],
