223c5fdd659c77822957c8b11a8cc94b31ddb56a
MOHAMED AZIZ GALAOUI <apopolo9800@gmail.com>
2026-09-19 14:22:49 +0100

    Keep package identity consistent

--- app/src/main/java/com/moneyflow/ui/screens/InsightsScreen.kt
+++ app/src/main/java/com/moneyflow/ui/screens/InsightsScreen.kt
@@ -1,4 +1,4 @@
-package com.moneyflow.ui.screens
+package com.tropico.moneyflow.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,11 +13,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.moneyflow.model.TrendRange
-import com.moneyflow.ui.components.DonutChart
-import com.moneyflow.ui.components.TrendLineChart
-import com.moneyflow.util.MoneyFormat
-import com.moneyflow.viewmodel.InsightsUiState
+import com.tropico.moneyflow.model.TrendRange
+import com.tropico.moneyflow.ui.components.DonutChart
+import com.tropico.moneyflow.ui.components.TrendLineChart
+import com.tropico.moneyflow.util.MoneyFormat
+import com.tropico.moneyflow.viewmodel.InsightsUiState
 
 @Composable
 fun InsightsScreen(state: InsightsUiState, onRangeChanged: (TrendRange) -> Unit) {
