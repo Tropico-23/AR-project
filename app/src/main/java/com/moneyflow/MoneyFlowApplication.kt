f92932a3f1f85d7d1f81b5d614c51bd6c4718510
MOHAMED AZIZ GALAOUI <apopolo9800@gmail.com>
2026-09-19 14:24:48 +0100

    Fix requested update integration and migration compatibility

--- app/src/main/java/com/moneyflow/MoneyFlowApplication.kt
+++ app/src/main/java/com/moneyflow/MoneyFlowApplication.kt
@@ -5,6 +5,10 @@ import androidx.room.Room
 import com.tropico.moneyflow.data.database.MoneyFlowDatabase
 import com.tropico.moneyflow.data.preferences.PreferencesRepository
 import com.tropico.moneyflow.data.repository.ExpenseRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
 class MoneyFlowApplication : Application() {
     lateinit var appContainer: AppContainer
